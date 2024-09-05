21e44111570ca9330ea0bd10f94dbf2acfba45e3
SpaceAnchor <1105110750@qq.com>
2024-09-05 15:30:40 +0800

    首次提交

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+
+module.exports = {
+  outputDir: 'dist',
+  assetsDir: 'assets',
+  css: {
+    extract: true,
+    sourceMap: false,
+  },
+  devServer: {
+    port: 80,
+    https: false,
+    open: false
+  },
+  lintOnSave: false,
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new CompressionPlugin({
+        algorithm: 'gzip',
+        test: /\.js$|\.html$|\.css$/,
+        filename: '[path].gz[query]',
+        minRatio: 1,
+        threshold: 10240,
+        deleteOriginalAssets: false
+      })
+    ]
+  }
+}
